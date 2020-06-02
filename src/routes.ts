5e6582cf9da241e1ff2768446795ac9b2cc06b07
Alberto Silva <alberto.silva5456@gmail.com>
2020-06-02 19:40:19 -0300

    creation of ItemsController and pointsController

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,15 @@
 import express from 'express'
-
+import PointsController from './controller/pointsController'
+import ItemsController from './controller/itemsController'
 const routes = express.Router()
 
-routes.get('/',(req,res)=>{
-    console.log('ola')
-})
+const pointsController = new PointsController()
+const itemsController = new ItemsController()
+//index, show, create, update, delete
+routes.get('/items', itemsController.index)
+
+routes.post('/points',pointsController.create)
+routes.get('/points',pointsController.index)
+routes.get('/points/:id',pointsController.show)
 
 export default routes
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import routes from './routes'
-
+import path from 'path'
 const app = express()
 
 app.use(express.json())
 app.use(routes)
+app.use('/uploads',express.static(path.resolve(__dirname,'..','uploads')))
 
 const port = process.env.port || 4000
 
